a8547bf7b13ae689b289cf117f435dbd62b2bbd8
Amin Mirzaei <aminmirzaei.og@gmail.com>
2024-10-21 23:35:16 +0330

    - Update and Change AssmblyInfo Project

--- WeatherUi/Properties/AssemblyInfo.cs
+++ WeatherUi/Properties/AssemblyInfo.cs
@@ -8,12 +8,12 @@ using System.Windows;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("WeatherUi")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Designing a weather program in WPF")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("AminMirzaeiOne")]
 [assembly: AssemblyProduct("WeatherUi")]
-[assembly: AssemblyCopyright("Copyright ©  2024")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("AminMirzaeiOne")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
